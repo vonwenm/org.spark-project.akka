5626aba2c5df3549006f0d3d36ebb7d0e02c23cb
Marcelle von Wendland <mvw@bancstreet.com>
2015-06-24 11:04:44 +0100

    initial

---
+++ install.bat
@@ -0,0 +1,18 @@
+cd actor
+mvn clean install
+cd ..
+cd testkit
+mvn clean install
+cd ..
+cd slf4j
+mvn clean install
+cd ..
+cd zeromq
+mvn clean install
+cd ..
+cd remote
+mvn clean install
+cd ..
+cd actor
+mvn clean install
+cd ..
